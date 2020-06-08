db6a1a90cf16eeb136b4929b7f049b955f55ded7
chaithra-harshi <chaithrabn96@gmail.com>
2020-06-09 01:30:07 +0530

    [Chaitra] Added: Project

---
+++ src/components/UserInformation.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class UserInformation extends Component {
+    render() {
+        return (
+            <div>
+              <h2> <span style={{ color: "#2196f3" }}>Check your Email for further Process</span>
+              </h2>
+            </div>
+        )
+    }
+}
